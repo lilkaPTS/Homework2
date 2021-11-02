400b1190931f9bce26e50d41c49538e95f86be62
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2021-11-02 22:53:45 +0300

    Main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,33 @@
 package com.company;
 
+import com.company.chapters.Chapter1;
+import com.company.enums.Exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("123");
-
-        Test test = new Test(){
-            private int id = 1;
-            private String name = "Ilya";
-
-
-        };
-
-        System.out.println(test);
-
+        Scanner in = new Scanner(System.in);
+        Arrays.asList(Exercises.values()).forEach(ex -> System.out.println((ex.ordinal()+1) + ") " + ex.name()));
+        while (true) {
+            System.out.println("Enter a number from 1 to " + (Exercises.values().length));
+            if (in.hasNextInt()) {
+                int item = in.nextInt();
+                if(item > 0 && item <= Exercises.values().length) {
+                    switch (Exercises.values()[item-1]) {
+                        case CHAPTER_1_EX1:
+                            Chapter1.numberTranslator();
+                            break;
+                        case CHAPTER_1_EX2:
+                            Chapter1.injectionChecker();
+                            break;
+                    }
+                    break;
+                }
+            }
+            in.nextLine();
+        }
     }
 }
--- src/com/company/Test.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Test {
-    @Override
-    public String toString() {
-        return "super.toString()";
-    }
-}
